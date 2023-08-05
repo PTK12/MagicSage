4fb37260aebfc05cc762ea10d8281ed90fd22664
PotatoK123 <56174807+PotatoK123@users.noreply.github.com>
2023-08-05 19:28:55 +0100

    dedent test

--- test/docstrings/prompt2.py
+++ test/docstrings/prompt2.py
@@ -9,7 +9,7 @@
 """           : punctuation.definition.string.begin.python, source.sage, string.quoted.docstring.multi.python
     def foo(): : source.sage, string.quoted.docstring.multi.python
               : source.sage, string.quoted.docstring.multi.python
-        ...   : keyword.control.flow.python, source.sage, string.quoted.docstring.multi.python
+...           : keyword.control.flow.python, source.sage, string.quoted.docstring.multi.python
               : source.sage, string.quoted.docstring.multi.python
-        >>>   : keyword.control.flow.python, source.sage, string.quoted.docstring.multi.python
+>>>           : keyword.control.flow.python, source.sage, string.quoted.docstring.multi.python
 """           : punctuation.definition.string.end.python, source.sage, string.quoted.docstring.multi.python
